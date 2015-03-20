f9a4c3bb8761d5edfc2664cfd44a7676dd4f71e1
alextryonpdx <alextryonpdx@gmail.com>
2015-03-20 13:11:05 -0700

    lunch
    fixed a little glitch in user registration. now UserProfile also
    created. need to redirect to login then to some page

--- blazerbar_project/urls.py
+++ blazerbar_project/urls.py
@@ -8,14 +8,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'blazerbar_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^blazerbar/games/$', views.get_games, name='get_games'),
     url(r'^/about/$', views.about, name="about"),
     url(r'^blazerbar/bars/$', views.get_bars, name='get_bars'),
     url(r'^blazerbar/teams/$', views.teams, name='teams'),
     url(r'^blazerbar/bar/(?P<bar_slug>[\w\-]+)/$', views.bar_page, name='bar_page'),
     url(r'^blazerbar/(?P<team_id>[\w\-]+)/$', views.get_games, name='get_games'),
-    url(r'^register/$', views.register, name='register'),
+    url(r'^register/', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout')
 
