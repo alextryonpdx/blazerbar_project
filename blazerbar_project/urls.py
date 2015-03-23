553f756dd6168644a80cd22ef341b456911025fc
alextryonpdx <alextryonpdx@gmail.com>
2015-03-23 16:02:04 -0700

    reversion
    got the old registration to work. creates a User and an unfilled
    UserProfile. need to redirect to new-profile page so the user can create
    their cool profile.

--- blazerbar_project/urls.py
+++ blazerbar_project/urls.py
@@ -2,11 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from blazerbar import views
 from django.conf import settings
-from registration.backends.simple.views import RegistrationView
-
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, request, user):
-        return '/blazerbar/bars'
 
 
 urlpatterns = patterns('',
@@ -17,27 +12,10 @@ urlpatterns = patterns('',
     url(r'^blazerbar/teams/$', views.teams, name='teams'),
     url(r'^blazerbar/bar/(?P<bar_slug>[\w\-]+)/$', views.bar_page, name='bar_page'),
     url(r'^blazerbar/team/(?P<team_id>[\w\-]+)/$', views.get_games, name='get_games'),
-<<<<<<< HEAD
-    #url(r'^blazerbar/register/', views.register, name='register'),
-    #url(r'^login/$', views.user_login, name='login'),
-    #url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^blazerbar/userprofile/(?P<username>[\w\-]+)/$', views.user_profile, name='userpage'),
-    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
-    #url(r'^blazerbar/', include('blazerbar_project.urls')),
-    (r'^accounts/', include('registration.backends.simple.urls')),
-=======
     url(r'^blazerbar/register/', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^logout/$', views.user_logout, name='logout')
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 0b10f9b... monday morning
-=======
->>>>>>> parent of 0b10f9b... monday morning
-=======
->>>>>>> parent of 0b10f9b... monday morning
-
-   # url(r'^blazerbar/friends/', views.get_friends, name='get_friends')
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^blazerbar/userprofile/(?P<username>[\w\-]+)/$', views.user_profile, name='userpage'),
     )
 
 
