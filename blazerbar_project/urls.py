1eeddc2c6fcbe054f3da48e14cb9f8038edd0340
alextryonpdx <alextryonpdx@gmail.com>
2015-03-23 15:06:28 -0700

    rabbit hole
    switching to django registration redux caused more problems than it
    solved at this time. maybe try again later

--- blazerbar_project/urls.py
+++ blazerbar_project/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from blazerbar import views
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return '/blazerbar/bars'
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blazerbar_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blazerbar/games/$', views.get_games, name='get_games'),
     url(r'^/about/$', views.about, name="about"),
@@ -15,13 +17,17 @@ urlpatterns = patterns('',
     url(r'^blazerbar/teams/$', views.teams, name='teams'),
     url(r'^blazerbar/bar/(?P<bar_slug>[\w\-]+)/$', views.bar_page, name='bar_page'),
     url(r'^blazerbar/team/(?P<team_id>[\w\-]+)/$', views.get_games, name='get_games'),
-    url(r'^blazerbar/register/', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^blazerbar/userprofile/(?P<username>[\w\-]+)/$', views.user_profile, name='userpage')
+    #url(r'^blazerbar/register/', views.register, name='register'),
+    #url(r'^login/$', views.user_login, name='login'),
+    #url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^blazerbar/userprofile/(?P<username>[\w\-]+)/$', views.user_profile, name='userpage'),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    #url(r'^blazerbar/', include('blazerbar_project.urls')),
+    (r'^accounts/', include('registration.backends.simple.urls')),
 
    # url(r'^blazerbar/friends/', views.get_friends, name='get_friends')
     )
+
 
 
  #url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
